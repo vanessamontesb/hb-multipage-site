068b5b2eab18271ab8559c54ff1862a8f66a682e
Vanessa Montes <vanessamontesb@gmail.com>
2018-01-31 12:19:50 -0500

    initial configuration

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const StyleLintPlugin = require('stylelint-webpack-plugin')
+const path = require('path')
+
+const config = {
+  entry: './src/main.js',
+  devtool: 'inline-sourceMap',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            'css-loader',
+            'sass-loader?sourceMap'
+          ]
+        })
+      },
+      {
+          // set up standard-loader as a preloader
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        loader: 'standard-loader',
+        exclude: /(node_modules|bower_components)/,
+        options: {
+            // Emit errors instead of warnings (default = false)
+          error: false,
+            // enable snazzy output (default = true)
+          snazzy: true
+        }
+      },
+      {
+        test: /\.pug$/,
+        use: [
+          {
+            loader: 'pug-loader'}
+        ]}
+    ]},
+  plugins: [
+    new StyleLintPlugin({}),
+    new ExtractTextPlugin('styles.css'),
+    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        beautify: false,
+        ecma: 6,
+        compress: true,
+        comments: false
+      }
+    })
+  ]
+}
+
+module.exports = config
